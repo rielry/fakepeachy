97e20952e12fe91729e62a55f5857be1cdbde70e
Riley Jhi <riley.jhi@gmail.com>
2019-08-06 01:49:46 -0500

    created nav toggle

--- src/components/Pages/Main/Main.tsx
+++ src/components/Pages/Main/Main.tsx
@@ -1,13 +1,36 @@
 import React from "react";
 import "./Main.scss";
 import Navigation from "../../Navigation/Navigation";
+import { PanelTypes } from "../../../constants/PageTypes";
 
-export default class Main extends React.Component {
+interface ComponentProps {
+    displayedPanel: PanelTypes;
+}
+
+type MainProps = ComponentProps;
+
+export default class Main extends React.Component<MainProps> {
     render() {
         return (
             <div className="Main">
                 <Navigation/>
+                <div className="Panel">
+                {this.getPanelContent()}
+                </div>
             </div>
         )
+    }
+
+    getPanelContent() {
+        switch(this.props.displayedPanel) {
+            case PanelTypes.ART:
+                return(<div>art</div>)
+            case PanelTypes.CODING:
+                return(<div>coding</div>)
+            case PanelTypes.RESUME:
+                return(<div>resume</div>)
+            default:
+                return(<div>about</div>)
+        }
     }
 }
