ec8ec8708e8cb25ccda207fc97418eeaa7aa9418
Riley Jhi <riley.jhi@gmail.com>
2019-08-05 22:51:16 -0500

    created sliding animation

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -1,2 +1,30 @@
 import React from "react";
+import "./Landing.scss";
+import { TextStyle } from "../../constants/TextStyles";
+import StyledText from "../StyledText/StyledText";
 
+export default class Landing extends React.Component {
+    componentDidMount() {
+        setTimeout(function() {
+            let enter = document.getElementById("Enter");
+            if(enter) {
+                enter.style.transform = "translateY(150px)";
+                enter.style.transition = "2s";
+            }
+        }, 0)
+        
+    }
+
+    render() {
+        return (
+            <div className="Entrance">
+                <div className="Nameplate">
+                    <StyledText style={TextStyle.HEADER}>Riley Jhi</StyledText>
+                </div>
+                <div className="Enter" id="Enter">
+                    <StyledText style={TextStyle.HEADER}>enter</StyledText>
+                </div>
+            </div>
+        );
+    }
+}
---
+++ src/constants/TextStyles.ts
@@ -0,0 +1,5 @@
+export enum TextStyle {
+    LABEL,
+    HEADER,
+    BODY
+}
