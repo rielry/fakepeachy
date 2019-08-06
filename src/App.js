8200fb7f9d9c6f8a85270af87c3c6731b857e0e4
Riley Jhi <riley.jhi@gmail.com>
2019-08-06 00:02:03 -0500

    added undux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Landing from "../src/components/Landing/Landing";
 import "../src/style/App.scss";
+import Store from "../src/store/store"
 
 function App() {
   return (
-    <div className="App">
-      <Landing/>
-    </div>
+    <Store.Container>
+      <div className="App">
+        <Landing/>
+      </div>
+    </Store.Container>
   );
 }
 
