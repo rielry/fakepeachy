98f5182f67a12e8ddac59cf6573595dd0aa14335
Riley Jhi <riley.jhi@gmail.com>
2019-08-07 00:50:44 -0500

    added some content

--- src/components/Pages/Home/Panels/About/About.tsx
+++ src/components/Pages/Home/Panels/About/About.tsx
@@ -3,6 +3,7 @@ import "./About.scss";
 import StyledText from "../../../../StyledText/StyledText";
 import { TextStyle } from "../../../../../constants/TextStyles";
 
+
 export default class About extends React.Component {
     render() {
         return (
@@ -16,9 +17,10 @@ export default class About extends React.Component {
                     </div>
                     <div className="Text">
                         <StyledText style={TextStyle.BODY}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et dui ligula. Vestibulum vestibulum iaculis sapien eu varius. Maecenas fermentum eu ante vel fermentum. Nam nec varius felis. Fusce commodo urna sed enim condimentum maximus. Proin scelerisque tempus sagittis. Pellentesque tristique magna vitae massa pharetra, et convallis felis feugiat. Morbi pharetra vitae velit vitae commodo. Mauris sagittis sit amet justo ac dictum. Nullam tristique sem sed erat mattis, pharetra scelerisque ex maximus. Maecenas tincidunt mauris vel massa consectetur, ut porta enim rhoncus. Sed sed diam sit amet nisl iaculis eleifend. Sed scelerisque pulvinar magna non commodo. Sed vitae diam non nunc viverra dictum.
-
-Nulla et ante nec eros auctor maximus. Suspendisse potenti. Aenean sed egestas libero. Sed magna sem, porttitor quis cursus eget, venenatis vel diam. Maecenas posuere quam risus, ac consectetur est fermentum nec. Aenean laoreet neque ut sapien auctor, a malesuada nisi ultrices. In ac placerat neque, id hendrerit ante. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id ultricies felis. Maecenas vel erat nisl. Curabitur porta nisl ut neque vehicula fermentum. Fusce nec odio libero. Integer felis felis, commodo in ex vel, sagittis dignissim sem. Nunc tempus tellus eu nibh molestie, sed accumsan ligula tincidunt. Duis ultricies, sem eget placerat finibus, augue eros luctus risus, non blandit dui libero vitae justo.
+                            Hi there, I'm so happy to see you here!  
+                            My name is Riley. I'm a senior at University of Nebraska - Lincoln, studying computer science and studio art.
+                            During my spare time, I like to paint, draw, play music, take photos, and of course, code!
+                            Shoot me a message at riley.jhi@gmail.com
                         </StyledText>
                     </div>
                 </div>
